439bec937ac43955291d11953f682afa0d756654
nivato <nivato@users.noreply.github.com>
2023-03-22 14:41:12 +0000

    deploy: nivato/actions-test@a87d22de06a56c7be385a977a513b271516e448d

--- application.js
+++ application.js
@@ -10,17 +10,19 @@
         return this;
     };
 
-    def.createElementWithClass = function(el, className){
+    def.createElementWithClass = function(el, className=""){
         let element = document.createElement(el);
-        element.className = className;
+        if (className){
+            element.className = className;
+        }
         return element;
     };
 
-    def.div = function(className){
+    def.div = function(className=""){
         return this.createElementWithClass('div', className);
     };
 
-    def.span = function(className){
+    def.span = function(className=""){
         return this.createElementWithClass('span', className);
     };
 
@@ -62,34 +64,118 @@
             icon.className = "t-link-icon";
             icon.src = "/images/" + iconImage;
             link.appendChild(icon);
-            link.innerHTML += " ";
+            link.innerHTML += "&nbsp;";
         }
         link.innerHTML += linkText;
         bold.appendChild(link);
         return bold;
     };
 
+    def.typingAnimation = function(terminalEntries=[]){
+        if (terminalEntries.length === 0){
+            return;
+        }
+        let isLastEntry = terminalEntries.length === 1;
+        let terminalEntry = terminalEntries.shift() || {};
+        let welcome = terminalEntry.welcome || [];
+        let command = terminalEntry.command;
+        let output = terminalEntry.output || [];
+        let lsLink = terminalEntry.lsLink;
+
+        let text = this.span();
+        let cursor = this.span("cursor");
+        cursor.innerHTML = "🁢";
+
+        if (command){
+            let line = this.createLine("");
+            line.appendChild(text);
+            line.appendChild(cursor);
+            this.tBody.appendChild(line);
+        }
+
+        let intervalId;
+        let counter = 0;
+        let terminal = this;
+        intervalId = setInterval(function(){
+            if (command && counter <= command.length) {
+                text.innerHTML = terminal.prompt + command.slice(0, counter).replaceAll("\"", "&quot;");
+                counter++;
+            } else if (welcome && counter < welcome.length){
+                terminal.addLine(welcome[counter]);
+                counter++;
+            } else {
+                clearInterval(intervalId);
+                intervalId = null;
+                setTimeout(function(){
+                    cursor.remove();
+                    output.forEach(function(outLine){
+                        terminal.addOutputLine(outLine);
+                    });
+                    if (lsLink){
+                        let linkLine = terminal.addOutputLine(
+                            "drwx------&nbsp;&nbsp;nivato&nbsp;&nbsp;admin&nbsp;&nbsp;736M&nbsp;&nbsp;",
+                        );
+                        let actionsTestLink = terminal.createTerminalLink(
+                            lsLink.linkText,
+                            lsLink.url,
+                            lsLink.iconImage,
+                        );
+                        linkLine.appendChild(actionsTestLink);
+                    }
+                    if (isLastEntry){
+                        terminal.addCommandLine("", true);
+                    }
+                    terminal.typingAnimation(terminalEntries);  // RECURSION!
+                }, 700);
+            }
+        }, 100);
+    };
+
     def.fillInTerminal = function(){
-        this.addLine("Last login: Fri Feb 10 17:30:13 on ttys001");
-        this.addLine();
-        this.addCommandLine("echo \"Hello, World\"'!'");
-        this.addOutputLine("Hello, World!");
-        this.addCommandLine();
-        this.addCommandLine();
-        this.addCommandLine("ls -lah");
-        this.addOutputLine("total 248");
-        this.addOutputLine();
-        let linkLine = this.addOutputLine(
-            "drwx------&nbsp;&nbsp;nivato&nbsp;&nbsp;admin&nbsp;&nbsp;736M&nbsp;&nbsp;",
-        );
-        let actionsTestLink = this.createTerminalLink(
-            "actions-test - Allure Reports",
-            "https://nivato.github.io/actions-test/",
-            "allure.png",
-        );
-        linkLine.appendChild(actionsTestLink);
-        this.addOutputLine();
-        this.addCommandLine("", true);
+        // TODO: The old "static" terminal contents
+        // this.addLine("Last login: Fri Feb 10 17:30:13 on ttys001");
+        // this.addLine();
+        // this.addCommandLine("echo \"Hello, World\"'!'");
+        // this.addOutputLine("Hello, World!");
+        // this.addCommandLine();
+        // this.addCommandLine();
+        // this.addCommandLine("ls -lah");
+        // this.addOutputLine("total 248");
+        // this.addOutputLine();
+        // let linkLine = this.addOutputLine(
+        //     "drwx------&nbsp;&nbsp;nivato&nbsp;&nbsp;admin&nbsp;&nbsp;736M&nbsp;&nbsp;",
+        // );
+        // let actionsTestLink = this.createTerminalLink(
+        //     "actions-test - Allure Reports",
+        //     "https://nivato.github.io/actions-test/",
+        //     "allure.png",
+        // );
+        // linkLine.appendChild(actionsTestLink);
+        // this.addOutputLine();
+        // this.addCommandLine("", true);
+
+        let terminalEntries = [
+            {welcome: ["Last login: Fri Feb 10 17:30:13 on ttys001", "&nbsp;"]},
+            {
+                command: "echo \"Hello, World\"'!'",
+                output: ["Hello, World!"],
+            },
+            {command: " "},
+            {command: " "},
+            {
+                command: "ls -lah",
+                output: ["total 248", ""],
+            },
+            {
+                lsLink: {
+                    linkText: "actions-test - Allure Reports",
+                    url: "https://nivato.github.io/actions-test/",
+                    iconImage: "allure.png",
+                },
+            },
+            {output: [""]},
+        ];
+        this.typingAnimation(terminalEntries);
     };
 
     let GhPage = function(){};
